192f27354ac0838140cfb6fc3691315bf60c0b11
patryk6496 <patrykbek07@gmail.com>
2023-03-28 15:26:41 +0200

    Repair deploy to netlify

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -94,77 +94,77 @@ export default function Example() {
 
       <div className="flex justify-center pt-28 ">
     <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
-      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage}/></div>
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
         <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
       
       
-        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce}/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
 
     </div>
   </div>
   
   <div className="flex justify-center mt-4 ">
   <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
-      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage}/></div>
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
         <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
       
       
-        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce}/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
 
     </div>
   </div>
 
   <div className="flex justify-center mt-4">
   <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
-      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage}/></div>
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
         <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
       
       
-        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce}/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
 
     </div>
   </div>
 
   <div className="flex justify-center mt-4">
   <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
-      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage}/></div>
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
         <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
       
       
-        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce}/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
 
     </div>
   </div>
 
   <div className="flex justify-center mt-4">
   <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
-      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage}/></div>
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
         <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
       
       
-        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce}/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
 
     </div>
   </div>
 
   <div className="flex justify-center mt-4">
   <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
-      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage}/></div>
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
         <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
       
       
-        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce}/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
 
     </div>
   </div>
 
   <div className="flex justify-center mt-4 pb-4">
   <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
-      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage}/></div>
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
         <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
       
       
-        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce}/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
 
     </div>
     </div>
