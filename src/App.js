d2f6e0347e8935dc9e16378bc561996c7dd9f7be
patryk6496 <patrykbek07@gmail.com>
2023-03-28 15:31:18 +0200

    Repair deploy to netlify v2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import './App.css';
 import RegisterForm from '../src/Components/RegisterForm';
 import LoginForm from '../src/Components/LoginForm';
-import CarList from './Components/CarList';
-import {Routes, Route, Link} from "react-router-dom"
+import {Routes, Route} from "react-router-dom"
 import Home from './Components/Home';
 import AddAnnouncement from './Components/AddAnnouncement';
 import Favorite from './Components/Favorite';
@@ -15,7 +14,6 @@ function App() {
       <Route path="/" element={<Home/>} />
       <Route path="/register" element={<RegisterForm/>} />
       <Route path="/login" element={<LoginForm/>} />
-      <Route path="/CarList" element={<CarList/>} />
       <Route path="/Home" element={<Home/>} />
       <Route path="/AddAnnouncement" element={<AddAnnouncement/>} />
       <Route path="/Favorite" element={<Favorite/>} />
