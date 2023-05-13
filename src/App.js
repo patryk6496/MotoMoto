ac5324075fc85a1c0e04237f869ffa13d143e55d
Patryk Bek <patrykbek07@gmail.com>
2023-05-13 22:33:31 +0200

    Add CarList Router

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {Routes, Route} from "react-router-dom"
 import Home from './Components/Home';
 import AddAnnouncement from './Components/AddAnnouncement';
 import Favorite from './Components/Favorite';
+import CarList from './Components/CarList'
+import Header from './Components/Header'
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
       <Route path="/Home" element={<Home/>} />
       <Route path="/AddAnnouncement" element={<AddAnnouncement/>} />
       <Route path="/Favorite" element={<Favorite/>} />
+	  <Route path="/CarList" element={<CarList/>} />
     </Routes>
     
 
--- src/Components/CarList.js
+++ src/Components/CarList.js
@@ -1,20 +1,86 @@
 import React from 'react';
+import volvoImage from '../../src/Assets/volvo.png';
+import serce from '../../src/Assets/serceOgloszenie.svg';
 
 export default function Example(){
     return(
         <>
-            <div class=" sm:rounded-lg bg-transparent text-md flex-col items-stretch md:h-4/5 md:w-4/5" bis_skin_checked="1">
-      <a href="/gosc/rezerwujId/180" class="flex flex-col items-center items-stretch bg-white my-1 border shadow-md md:rounded-lg md:flex-row md:w-full md:flex-row hover:bg-gray-100 md:items-stretch" bis_size="{&quot;x&quot;:190,&quot;y&quot;:96,&quot;w&quot;:1522,&quot;h&quot;:509,&quot;abs_x&quot;:190,&quot;abs_y&quot;:96}"/>
-        <img class="object-cover w-full h-fit md:h-4/5 md:w-1/2 md:rounded-none md:rounded-l-lg" alt = 'cos'src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/246453301.jpg?k=e1bffc28064c0b582a897ff8ef6d09ba0755d8cd9c186cb593b658023f770952&amp;o=&amp;hp=1" bis_size="{&quot;x&quot;:191,&quot;y&quot;:97,&quot;w&quot;:760,&quot;h&quot;:507,&quot;abs_x&quot;:191,&quot;abs_y&quot;:97}" bis_id="bn_1t8zndpeeet8rd3afv9j43"/>
-        <div class="flex flex-col justify-between p-4 md:w-1/2 leading-normal " bis_skin_checked="1">
-          <div calss="row" bis_skin_checked="1">
-            <h5 class="col mb-2 text-2xl font-bold tracking-tight text-gray-900 "><label>Numer Pokoju: 100</label></h5>
-            <label class="col">Cena za noc: 300.00zł</label>
-          </div>
-            <p class="row mb-3 font-normal text-gray-700 "><label>Apartament typu Suite z 1 sypialnią i balkonem </label></p>
-        </div>
-        </div>
-        
+            <div className="flex justify-center pt-28 ">
+    <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
+        <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
+      
+      
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+
+    </div>
+  </div>
+  
+  <div className="flex justify-center mt-4 ">
+  <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
+        <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
+      
+      
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+
+    </div>
+  </div>
+
+  <div className="flex justify-center mt-4">
+  <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
+        <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
+      
+      
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+
+    </div>
+  </div>
+
+  <div className="flex justify-center mt-4">
+  <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
+        <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
+      
+      
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+
+    </div>
+  </div>
+
+  <div className="flex justify-center mt-4">
+  <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
+        <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
+      
+      
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+
+    </div>
+  </div>
+
+  <div className="flex justify-center mt-4">
+  <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
+        <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
+      
+      
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+
+    </div>
+  </div>
+
+  <div className="flex justify-center mt-4 pb-4">
+  <div className="w-9/12 h-50 rounded-lg bg-black flex justify-between ">
+      <div className="w-1/4 text-white m-2.5 " ><img src={volvoImage} alt='carImage'/></div>
+        <div className = "text-white w-7/12 m-0 py-2.5"><h2 className='text-3xl'> Volvo XC 90 D5 2019</h2><p className='text-xl'>98 000 km, 1 969 cm3, 235 KM, Diesel</p></div>
+      
+      
+        <div className = "text-white w-56 flex float-right py-2.5 " ><button className='flex pr-2.5'><img className='w-7 h-9' src={serce} alt='heartIcon'/></button><p className='text-3xl py-0.5'>25000 PLN</p></div>
+
+    </div>
+    </div>
         </>
     )
 }
