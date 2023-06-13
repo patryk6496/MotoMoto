958d0c9439f2fdb4e4ecae375281ba5f5fe88244
NSN312 <neversaynever3142@gmail.com>
2023-06-13 02:37:46 +0200

    linked to database and added redirect to loginpage

--- src/Components/RegisterForm.js
+++ src/Components/RegisterForm.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from "react-router-dom"
+
 import logo2 from '../Assets/Logo.svg'
 import Zarejestrujicon from '../Assets/zarejestrujIcon.svg'
-import {Link} from "react-router-dom"
 
 export default function Example() {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Serialize the form data
+    const formData = new FormData(event.target);
+    const formObject = {};
+    formData.forEach((value, key) => {
+      formObject[key] = value;
+    });
+    const serializedData = Object.entries(formObject)
+      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+      .join('&');
+
+    try {
+      const response = await axios.post('http://localhost:8080/auth/register', serializedData);
+      // Handle response
+      console.log(response.status);
+      navigate('/login'); // Navigate to '/home' after form submission
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="flex min-h-screen items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="w-full max-w-md space-y-8">
           <div>
-          <Link to="/home"><img className="mx-auto h-12 w-auto" src={logo2} alt="Moto Moto"/></Link>
-            
+            <Link to="/home"><img className="mx-auto h-12 w-auto" src={logo2} alt="Moto Moto" /></Link>
+
           </div>
-          <form className="mt-8 space-y-6" action="#" method="POST">
+          <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
             <input type="hidden" name="remember" defaultValue="true" />
             <div className="-space-y-px rounded-md shadow-sm">
               <div >
@@ -49,8 +76,8 @@ export default function Example() {
                   Retype Password
                 </label>
                 <input
-                  id="password"
-                  name="password"
+                  id="password2"
+                  name="password2"
                   type="password"
                   autoComplete="current-password"
                   required
@@ -60,24 +87,24 @@ export default function Example() {
               </div>
 
               <div className='pt-4'>
-              <label htmlFor="phone">
-              Telefon
-              </label>
+                <label htmlFor="phone">
+                  Telefon
+                </label>
                 <input
                   id="phone"
                   name="phone"
                   type="tel"
                   required
                   className="relative block w-full rounded-lg border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 
-                    placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Numer telefonu"
-                  pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+                    placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Numer telefonu (000-000-000)"
+                  pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}"
                 />
               </div>
 
               <div className='pt-4'>
-              <label htmlFor="nick">
-              Nick
-              </label>
+                <label htmlFor="nick">
+                  Nick
+                </label>
                 <input
                   id="nick"
                   name="nick"
@@ -89,21 +116,21 @@ export default function Example() {
               </div>
             </div>
             <div>
-            <div className='w-32 mx-auto mt-9' >
-              <button
-                type="submit"
-                className="group relative flex w-32 m-0 justify-center rounded-md bg-indigo-600 pt-3 pb-2 px-3 text-sm  font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                
-                Zarejestruj
-                <img 
-                  className="mx-0 h-6 w-auto"
-                  src={Zarejestrujicon}
-                  alt="Moto Moto"
-                 />
-              </button>
-              
-            </div>
+              <div className='w-32 mx-auto mt-9' >
+                <button
+                  type="submit"
+                  className="group relative flex w-32 m-0 justify-center rounded-md bg-indigo-600 pt-3 pb-2 px-3 text-sm  font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+
+                  Zarejestruj
+                  <img
+                    className="mx-0 h-6 w-auto"
+                    src={Zarejestrujicon}
+                    alt="Moto Moto"
+                  />
+                </button>
+
+              </div>
             </div>
           </form>
         </div>
